c45a82f5ad3f196047eb19f654e416c443e937fb
MaxKiv <maxkivits42@gmail.com>
2025-03-25 11:48:25 +0100

    feat(nix): add nix devshell flake

---
+++ flake.nix
@@ -0,0 +1,34 @@
+{
+  description = "Devshell for qmk";
+
+  inputs = {
+    your-nixos-flake.url = "github:maxkiv/nix";
+    nixpkgs.follows = "your-nixos-flake/nixpkgs";
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  # Outputs this flake produces
+  outputs = {
+    self,
+    nixpkgs,
+    flake-utils,
+    ...
+  } @ inputs:
+    flake-utils.lib.eachDefaultSystem (system: let
+      pkgs = (import nixpkgs) {
+        inherit system;
+      };
+    in {
+      # Development shells provided by this flake, to use:
+      # nix develop .#default
+      devShell = pkgs.mkShell {
+        buildInputs = with pkgs; [
+          nil # nix LSP
+          alejandra # nix Formatter
+          qmk # cmdline helper tool for qmk
+        ];
+      };
+
+      formatter.x86_64-linux = nixpkgs.legacyPackages.x86_64-linux.alejandra;
+    });
+}
